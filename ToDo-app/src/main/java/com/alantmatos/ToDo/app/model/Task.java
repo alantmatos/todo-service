2ab40b512503015642dea3a9af2fc531fe71c3a6
alantmatos <mr.alanmatos@hotmail.com>
2023-02-17 07:40:20 -0500

    model

---
+++ ToDo-app/src/main/java/com/alantmatos/ToDo/app/model/Task.java
@@ -0,0 +1,57 @@
+package com.alantmatos.ToDo.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String name;
+    private String description;
+    private  boolean isActive;
+
+    public Task(int id, String name, String description, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isActive = isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
